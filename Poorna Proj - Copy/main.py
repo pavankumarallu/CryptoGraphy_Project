eb4eca6640da827a26a19b538046d3d29878680e
pavankumarallu <vamsipavankumarallu@gmail.com>
2022-04-17 15:08:09 +0530

    first commit

---
+++ Poorna Proj - Copy/main.py
@@ -0,0 +1,25 @@
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+
+from PySide2.QtWidgets import *
+
+from UI.LandingPage import *
+
+from os import environ
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    window = MainWindow()
+    sys.exit(app.exec_())
